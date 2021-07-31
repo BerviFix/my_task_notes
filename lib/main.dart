eaec2d175adf02f070d75935176e7e5cb466dc7c
Simone <simone.bervi@gmail.com>
2021-07-31 19:53:51 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class Day {
+  final int nr;
+  String? task;
+  Day(this.nr);
+}
+
+class Month {
+  final String name;
+  List<Day>? days;
+
+  Month(this.name, int daysNr) {
+    days = List.generate(daysNr, (daysNr) => Day(daysNr + 1));
+  }
+}
+
+final months = [
+  Month('Gennaio', 31),
+  Month('Febbraio', 28),
+  Month('Marzo', 31),
+  Month('Aprile', 30),
+  Month('Maggio', 31),
+  Month('Giugno', 30),
+  Month('Luglio', 31),
+  Month('Agosto', 31),
+  Month('Settembre', 30),
+  Month('Ottobre', 31),
+  Month('Novembre', 30),
+  Month('Dicembre', 31),
+];
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.limeAccent),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedMonthIndex = 0;
+
+  void onMonthSelected(int index) {
+    setState(() {
+      _selectedMonthIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calendar'),
+        centerTitle: true,
+      ),
+      drawer: Container(
+        width: 300,
+        color: Colors.white,
+        child: SafeArea(
+          child: ListView.separated(
+              itemBuilder: (context, index) {
+                return Container(
+                  color: index == _selectedMonthIndex
+                      ? Colors.grey.shade300
+                      : Colors.transparent,
+                  child: ListTile(
+                    onTap: () {
+                      onMonthSelected(index);
+                    },
+                    title: Text(
+                      months[index].name,
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return Container(
+                  width: double.infinity,
+                  height: 1,
+                  color: Colors.grey.shade300,
+                );
+              },
+              itemCount: months.length),
+        ),
+      ),
+    );
+  }
+}
